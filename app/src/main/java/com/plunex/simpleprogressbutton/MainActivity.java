3e4d672d903d8f0a7cfb485d9906dd35d44cf3d9
Daniel <oluwabukunmi.aluko@gmail.com>
2019-08-19 20:06:53 +0100

    Copied code from the old project into the example app, and made use of the library in the example app. Note: from a blog post, to avoid people overriding an android library resource, properly name the resource so it cannot be guessed.

--- app/src/main/java/com/plunex/simpleprogressbutton/MainActivity.java
+++ app/src/main/java/com/plunex/simpleprogressbutton/MainActivity.java
@@ -1,13 +1,62 @@
 package com.plunex.simpleprogressbutton;
 
-import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button mNormalBtn;
+    private Button mLoadingBtn;
+    private Button mPassedBtn;
+    private Button mRetryBtn;
+
+    private SimpleProgressButton simpleProgressButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        simpleProgressButton = findViewById(R.id.simpleProgressButton);
+
+        mNormalBtn = findViewById(R.id.button);
+        mLoadingBtn = findViewById(R.id.button1);
+        mPassedBtn = findViewById(R.id.button2);
+        mRetryBtn = findViewById(R.id.button3);
+
+        mNormalBtn.setOnClickListener(this);
+        mLoadingBtn.setOnClickListener(this);
+        mPassedBtn.setOnClickListener(this);
+        mRetryBtn.setOnClickListener(this);
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        int x = v.getId();
+        switch (x) {
+            case R.id.button:
+
+                simpleProgressButton.showDefault();
+                break;
+            case R.id.button1:
+                simpleProgressButton.showDefaultProgress("Posting");
+
+                break;
+            case R.id.button2:
+                simpleProgressButton.showCustomMessage("Posted", R.drawable.ic_checked);
+                break;
+
+            case R.id.button3:
+                simpleProgressButton.showCustomMessage("Retry", 0);
+                break;
+
+            default:
+                break;
+        }
     }
 }
