1d59902f7b48f0ec7f71386eae08cd8341674285
Daniel <oluwabukunmi.aluko@gmail.com>
2019-08-19 19:47:08 +0100

    Created the SimpleProgressButton class and extended linear layout.

---
+++ simpleprogressbutton/src/main/java/com/plunex/simpleprogressbutton/SimpleProgressButton.java
@@ -0,0 +1,21 @@
+package com.plunex.simpleprogressbutton;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.LinearLayout;
+
+import androidx.annotation.Nullable;
+
+public class SimpleProgressButton extends LinearLayout {
+    public SimpleProgressButton(Context context) {
+        super(context);
+    }
+
+    public SimpleProgressButton(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public SimpleProgressButton(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
